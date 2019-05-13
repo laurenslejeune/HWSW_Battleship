59c0329addc53d153fd24062dcba8274ff482aee
Laurens <laurens@noreply.com>
2019-05-13 13:51:16 +0200

    code changes

--- Definitief/software/comm/main.c
+++ Definitief/software/comm/main.c
@@ -6,10 +6,15 @@ int main()
 	int * master = MASTERSLAVE_0_BASE;
 	while(1)
 	{
+		if(master)
+		{
+			//*comm = *comm;
+			*comm = 63;
+		}
 		//int test = *comm;
 		//*comm = (*master << 5) + (*master << 4) + (*master << 3) + (*master << 2) + (*master << 1) + (*master << 0);
-		int test = *comm;
-		*comm = test;
+		//int test = *comm;
+		//*comm = test;
 	}
 	return 0;
 }
